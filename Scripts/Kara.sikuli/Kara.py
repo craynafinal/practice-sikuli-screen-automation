eaabf0929405f1ce6d003c6474dd8cff17b84629
craynafinal <craynafinal@gmail.com>
2017-10-07 07:42:37 -0700

    fixing loop logics

--- Scripts/Kara.sikuli/Kara.py
+++ Scripts/Kara.sikuli/Kara.py
@@ -9,15 +9,14 @@ def runHotkey(event):
 Env.addHotkey(Key.F1, KeyModifier.ALT, runHotkey)
 
 while(running):
-    if (exists(Pattern("1502652749997.png").similar(0.50)) or exists(Pattern("1502652648894.png").similar(0.52)) or count == 10):
+    if (exists(Pattern("1507354727310.png").similar(0.80)) or exists("1507354826038.png") or count == 10):
         
-        wait(1.5 *  count + 1)
+        wait(1.5 *  count + 2)
         type(Key.ENTER)
         count = 0
     else:
         count += 1
     wait(1)
-        
         
         
         

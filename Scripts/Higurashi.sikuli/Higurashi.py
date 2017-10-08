a673cf38441be9b55ff569f9807ef3e0bf517712
craynafinal <craynafinal@gmail.com>
2017-10-08 11:18:35 -0700

    modified higurashi automation logic

--- Scripts/Higurashi.sikuli/Higurashi.py
+++ Scripts/Higurashi.sikuli/Higurashi.py
@@ -9,8 +9,15 @@ def runHotkey(event):
 Env.addHotkey(Key.F1, KeyModifier.ALT, runHotkey)
 
 while(running):
-    if (exists(Pattern("1502652749997.png").similar(0.50)) or exists(Pattern("1502652648894.png").similar(0.52)) or count == 10):
-        
+    if (exists("1507485878000.png", .5)):
+        #If tip window shows up, wait
+        wait(3)
+        #Finding element in second screen
+        scr1 = Screen(1)
+        scr1.click("1507486006667.png")
+        exit()
+    elif (exists(Pattern("1507484956784.png").similar(0.50), .5) or exists(Pattern("1507485545706.png").similar(0.55), .5) or exists("1507484935580.png") or count == 10):
+        #Regular sequence
         wait(1.5 *  count + 1)
         type(Key.ENTER)
         count = 0

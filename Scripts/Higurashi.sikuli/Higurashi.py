6da4b5805d4a8944d701098a946c8f3e0791aef1
craynafinal <craynafinal@gmail.com>
2017-11-17 20:53:27 -0800

    adding jinkai and higurashi2 fix

--- Scripts/Higurashi.sikuli/Higurashi.py
+++ Scripts/Higurashi.sikuli/Higurashi.py
@@ -10,22 +10,22 @@ def runHotkey(event):
 Env.addHotkey(Key.F1, KeyModifier.ALT, runHotkey)
 
 while(running):
-    if ((exists("1507485878000.png", .5) or (exists("1509668195740.png", .5))) and scr1.exists(Pattern("1507491127022.png").exact(), .5)):
+    if ((exists("1507485878000.png", .3) or (exists("1509668195740.png", .3))) and scr1.exists(Pattern("1507491127022.png").exact(), .3)):
         #If tip window shows up, wait
-        wait(3)
+        wait(2)
         #Finding element in second screen
         scr1.doubleClick("1507486006667.png")
         type(Key.F7)
         
         exit()
-    elif (exists(Pattern("1507484956784.png").similar(0.50), .5) or exists(Pattern("1507485545706.png").similar(0.55), .5) or exists("1507484935580.png") or count == 10):
+    elif (exists(Pattern("1507484956784.png").similar(0.50), .3) or exists(Pattern("1507485545706.png").similar(0.55), .3) or exists("1507484935580.png", .3) or count == 10):
         #Regular sequence
-        wait(1.5 *  count + 1)
+        wait(.03 * (count + 1))
         type(Key.ENTER)
         count = 0
     else:
         count += 1
-    wait(1)
+    wait(.05)
         
         
         
--- Scripts/Jingai.sikuli/Jingai.py
+++ Scripts/Jingai.sikuli/Jingai.py
@@ -1,6 +1,5 @@
 Settings.MoveMouseDelay = 0
 running = True
-count = 0
 scr1 = Screen(1)
 
 def runHotkey(event):
@@ -10,18 +9,11 @@ def runHotkey(event):
 Env.addHotkey(Key.F1, KeyModifier.ALT, runHotkey)
 
 while(running):
-    if ((exists("1507485878000.png", .3) or (exists("1509668195740.png", .3))) and scr1.exists(Pattern("1507491127022.png").exact(), .3)):
+    if ((exists("1510798837938.png") or exists("1510889800736.png")) and scr1.exists(Pattern("1507491127022.png").exact(), .3)):
         #If tip window shows up, wait
-        wait(3)
+        wait(2)
         #Finding element in second screen
         scr1.doubleClick("1507486006667.png")
         type(Key.F7)
         
         exit()
-    else:
-        wait(.3)
-        type(Key.ENTER)
-        
-        
-        
-        

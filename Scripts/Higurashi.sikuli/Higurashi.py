b8dcc22e9415dda867d7e86217d783c3cc79b069
craynafinal <craynafinal@gmail.com>
2017-11-18 23:07:39 -0800

    higurashi timing fix

--- Scripts/Higurashi.sikuli/Higurashi.py
+++ Scripts/Higurashi.sikuli/Higurashi.py
@@ -10,7 +10,7 @@ def runHotkey(event):
 Env.addHotkey(Key.F1, KeyModifier.ALT, runHotkey)
 
 while(running):
-    if ((exists("1507485878000.png", .3) or (exists("1509668195740.png", .3))) and scr1.exists(Pattern("1507491127022.png").exact(), .3)):
+    if ((exists("1507485878000.png", .05) or (exists("1509668195740.png", .05))) and scr1.exists(Pattern("1507491127022.png").exact(), .1)):
         #If tip window shows up, wait
         wait(2)
         #Finding element in second screen
@@ -18,14 +18,14 @@ while(running):
         type(Key.F7)
         
         exit()
-    elif (exists(Pattern("1507484956784.png").similar(0.50), .3) or exists(Pattern("1507485545706.png").similar(0.55), .3) or exists("1507484935580.png", .3) or count == 10):
+    elif (exists(Pattern("1507484956784.png").similar(0.50), .03) or exists(Pattern("1507485545706.png").similar(0.55), .03) or exists("1507484935580.png", .03) or count == 10):
         #Regular sequence
-        wait(.03 * (count + 1))
+        wait(.03)
         type(Key.ENTER)
         count = 0
     else:
         count += 1
-    wait(.05)
+    wait(.03)
         
         
         
